d243b4025ac0723c4d7335e75b452f35c75eba7b
Aleksandar Misovic <aleksandar.misovic72@gmail.com>
2021-11-14 20:24:38 +0100

    cosmetics in app responses

--- app.js
+++ app.js
@@ -9,12 +9,17 @@ var handler = function(request, response) {
   console.log("Received request from " + request.connection.remoteAddress);
   requestCount++;
   response.writeHead(200);
-  response.write("VERSION: Version 1.4 - pushed at 20:22! " + "\n");
   response.write("BUILD_NUM:  "  + process.env.BUILD_NUM + "\n");
-  response.write("RESPOSNSE_MSG:  "  + process.env.RESPOSNSE_MSG + "\n");
+  response.write("=============================================== \n");
+  response.write(" \n");
+  response.write("VERSION: Version 1.4 - pushed at 20:22! " + "\n");
   response.write("Request Count: " + requestCount + "\n");
   response.write("Request comes from IP: " + request.connection.remoteAddress + "\n");
   response.end("You've hit " + os.hostname() + "\n");
+  response.write(" \n");
+  response.write("=============================================== \n");
+  response.write("RESPOSNSE_MSG:  "  + process.env.RESPOSNSE_MSG + "\n");
+
 };
 
 var www = http.createServer(handler);
